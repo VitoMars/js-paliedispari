3d7488d403faf88c1cf9be484fb15126b2f8455a
Vito <vito95v@gmail.com>
2021-07-23 15:00:14 +0200

    Push esercizio Palindrome

---
+++ Palindroma/js/main.js
@@ -0,0 +1,20 @@
+// ------Palidroma------
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+var string = prompt("Inserisci una parola e ti dirò se è palindroma");
+
+console.log(string);
+
+function checkPalindroma(parola) {
+  let parolaInvertita = parola.split("").reverse().join("");
+  if (parola == parolaInvertita) {
+    console.log("La parola è palindroma");
+    return true;
+  } else {
+    console.log("La parola non è palindroma");
+    return false;
+  }
+}
+
+checkPalindroma(string);
